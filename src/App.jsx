edec9b72000637117adfb0ff067f770fd4849b4f
DevMasterX <sokol080288@gmail.com>
2026-01-12 01:50:32 +0200

    6666999987

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,3 @@
-import { useState } from 'react';
-
-import './App.css';
-
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <main className="section container">
-      <div className="flex-center">
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="grid-center">
-        <h1 className="text-center">Vite + React + SWC + ESLint + Prettier + alias @</h1>
-        <div className="card grid-center">
-          <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      </div>
-    </main>
-  );
+export default function App() {
+  return null;
 }
-
-export default App;
